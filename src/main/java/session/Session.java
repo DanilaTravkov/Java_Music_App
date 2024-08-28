88c79478ae62938177b3abda72956ac1fbe8138b
DanilaTravkov <ygnbeats@gmail.com>
2024-08-28 13:17:31 +0200

    Created session as a singleton

--- src/main/java/session/Session.java
+++ src/main/java/session/Session.java
@@ -2,12 +2,23 @@ package session;
 import model.User;
 
 public class Session {
-    private static Session session;
+    private static Session instance;
     private static User userObject;
 
     public Session() {}
 
-    public static User getLoggedInUser() {
+    public static synchronized Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
+
+    public User getLoggedInUser() {
         return userObject;
+    }
+
+    public void setLoggedInUser(User user) {
+        userObject = user;
     }
 }
